8b343c56f0ff0de6dec0db132b359f6b5bc34040
i-rozhkov <gather.all@bk.ru>
2017-12-28 00:09:04 +0300

    Carousel was added. Created home page to gather components.

--- src/app.js
+++ src/app.js
@@ -3,16 +3,10 @@ import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
 import './css/styles.css';
 import NotFound from './components/NotFound';
 import './img/favicon.ico';
-
-class Home extends React.Component {
-	render() {
-		return (<Header />);
-	}
-}
+import Home from './components/Home';
 
 ReactDOM.render(
 	(
